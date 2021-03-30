54fb86de990aef92ecdec43fa690408f018635bb
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-30 06:20:16 -0400

    added a date post route

--- controllers/api/dateRoutes.js
+++ controllers/api/dateRoutes.js
@@ -1,30 +1,33 @@
 const router = require('express').Router();
 const session = require('express-session');
-const { User, Owner, Dog, Date } = require('../../models');
+const { User, Owner, Dog, Date, OwnerDate } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
 router.post('/',  async (req, res) => {
+  const ownerDateData = {};
   try {
       console.log(req.body);
-    const dateData = await Date.create(req.body).then(dateResponse, async () => {
-      if (!dateResponse) {
+    const dateData = await Date.create(req.body)
+    // .then(dateResponse, async () => {
+      if (!dateData) {
           res.status(400).json(err);
       } else {
-          ownerDateData = {
-              owner1_id: dateResponse.participant1_id,
-              owner2_id: dateResponse.participant2_id,
-              date_id: dateResponse.id
+          ownerDateData.owner1_id = dateData.dataValues.participant1_id;
+          ownerDateData.owner2_id = dateData.dataValues.participant2_id;
+          ownerDateData.date_id = dateData.dataValues.id;
           }
-          await OwnerDog.create(ownerDogData);
-          return dateResponse;
-      }
-  });;
-    res.status(200).json(dateData);
-    
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+          await OwnerDate.create(ownerDateData);
+          res.status(200).json(dateData);
+          // return dateData;
+        } catch (err) {
+          err => console.error(err);
+        }
+  
+      }) ;
+  // } catch (err) {
+  //   res.status(400).json(err);
+  // }
+// });
 
 module.exports = router;
