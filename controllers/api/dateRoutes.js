97aeef378b5d339271e0860342d3ed209da62042
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-30 09:44:46 -0400

    added dates search route for today

--- controllers/api/dateRoutes.js
+++ controllers/api/dateRoutes.js
@@ -102,4 +102,41 @@ console.log(dateData);
   }
 });
 
+router.get('/search', async (req, res) => {
+  sessionDate = new Date.now();
+  try {
+    const dateData = await Date.findAll({where: {date: sessionDate }},
+     {
+  //    include: [{ model: 'owner', as: 'dateOwner' }],
+      include: [{ all: true, nested: true }],
+   });
+  //  const dateData2 = await Date.findAll({where: {participant2_id: req.params.id }},
+    // {
+ //    include: [{ model: 'owner', as: 'dateOwner' }],
+    //  include: [{ all: true, nested: true }],
+  // });
+console.log(dateData);
+    if (!dateData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect date, please try again' });
+      return;
+    } else {
+      //   const dateSimpleData = dateData.map((date) =>
+      //    date.get({ plain: true }));
+      //    console.log(dateSimpleData);
+        res
+          .render('datebyzip', {
+              // owner_data: dateData.dateOwner.dataValues,
+              date_data: dateData.datavalues,
+              // date_data2: dateData2.datavalues,
+              logged_in: req.session.logged_in,
+          });
+    }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
