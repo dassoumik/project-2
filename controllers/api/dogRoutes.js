af74f5ce9f6d9bc8ea196d6eb421b5836500668f
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-29 21:53:20 -0400

    added get dogs by id route

--- controllers/api/dogRoutes.js
+++ controllers/api/dogRoutes.js
@@ -14,4 +14,34 @@ router.post('/',  async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+      const dogData = await Dog.findByPk(req.params.id,
+       {
+    //    include: [{ model: 'owner', as: 'dogOwner' }],
+        include: [{ all: true, nested: true }],
+     });
+  console.log(dogData);
+      if (!dogData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect dog, please try again' });
+        return;
+      } else {
+        //   const dogSimpleData = dogData.map((dog) =>
+        //    dog.get({ plain: true }));
+        //    console.log(dogSimpleData);
+          res
+            .render('dogpage', {
+                owner_data: dog.dogOwner.dataValues,
+                dog_data: dog.datavalues,
+                logged_in: req.session.logged_in,
+            });
+      }
+  
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
