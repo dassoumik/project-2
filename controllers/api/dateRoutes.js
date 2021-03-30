c42052e9f1b80e8d633f3035b50360efe42b4a50
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-30 06:31:34 -0400

    added get date data by owners id

--- controllers/api/dateRoutes.js
+++ controllers/api/dateRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const session = require('express-session');
-const { User, Owner, Dog, Date, OwnerDate } = require('../../models');
+const { User, Owner, Date, OwnerDate } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
@@ -29,5 +29,41 @@ router.post('/',  async (req, res) => {
   //   res.status(400).json(err);
   // }
 // });
+
+router.get('/:id', async (req, res) => {
+  try {
+    const dateData = await Date.findAll({where: {participant1_id: req.params.id }},
+     {
+  //    include: [{ model: 'owner', as: 'dateOwner' }],
+      include: [{ all: true, nested: true }],
+   });
+   const dateData2 = await Date.findAll({where: {participant2_id: req.params.id }},
+    {
+ //    include: [{ model: 'owner', as: 'dateOwner' }],
+     include: [{ all: true, nested: true }],
+  });
+console.log(dateData);
+    if (!dateData && !dateData2) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect date, please try again' });
+      return;
+    } else {
+      //   const dateSimpleData = dateData.map((date) =>
+      //    date.get({ plain: true }));
+      //    console.log(dateSimpleData);
+        res
+          .render('datepage', {
+              // owner_data: dateData.dateOwner.dataValues,
+              date_data: dateData.datavalues,
+              date_data2: dateData2.datavalues,
+              logged_in: req.session.logged_in,
+          });
+    }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
